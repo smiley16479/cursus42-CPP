40bcb3491aadd48fad01f93233ddd690b83e0df1
smiley16479 <smiley16479@aol.com>
2021-03-28 15:20:37 +0200

    module_3

--- cpp03/ex00/FragTrap.cpp
+++ cpp03/ex00/FragTrap.cpp
@@ -1,8 +1,16 @@
 #include "FragTrap.hpp"
 
-FragTrap::FragTrap( void ) : _hitPoints( 0 )
+FragTrap::FragTrap( std::string name ) : _hitPoints( 100 ), _maxHitPoints( 100 ), _energyPoints( 100 ),
+_maxEnergyPoints( 100 ), _level( 1 ), _name( name ), _melleeAttackDamage( 30 ), _rangedAttackDamage( 20 ),
+_armorDamageReduction( 5 )
 {
 	std::cout << "Default Constructor called" << std::endl;
+	_tabAttack[0] = rangedAttack;
+	_tabAttack[1] = meleeAttack;
+	_tabAttack[2] = provokeAttack;
+	_tabAttack[3] = fingerAttack;
+	_tabAttack[4] = hammerAttack;
+	srand (time(NULL));
 	return ;
 }
 
@@ -25,21 +33,90 @@ FragTrap::~FragTrap()
 	return ;
 }
 
-int FragTrap::getNb( void ) const
+int FragTrap::getHealth( void ) const
 {
-	return this->_hitPoints;
+	return _energyPoints;
 }
 
 FragTrap &    FragTrap::operator=( FragTrap const & rhs )
 {
 	std::cout << "Assignment operator called" << std::endl;
-	if (this != &rhs )
-		this->_hitPoints = rhs.getNb();
+	if ( this != &rhs ) {
+		_hitPoints = rhs._hitPoints;
+		_maxHitPoints = rhs._maxHitPoints;
+		_energyPoints = rhs._energyPoints;
+		_maxEnergyPoints = rhs._maxEnergyPoints;
+		_level = rhs._level;
+		_name = rhs._name;
+		_melleeAttackDamage = rhs._melleeAttackDamage;
+		_rangedAttackDamage = rhs._rangedAttackDamage;
+		_armorDamageReduction = rhs._armorDamageReduction;
+		for (int i = 0; i < 5; ++i)
+			_tabAttack[i] = rhs._tabAttack[i];
+	}
 	return *this;
+}
+
+void	FragTrap::vaulthunter_dot_exe(std::string const & target)
+{
+	// (FragTrap::_tabAttack[rand() % 5])(target);
+	FragTrap &fred = *this;
+	std::invoke(this->_tabAttack[rand() % 5],fred, target);
+	//  ((this).*(_tabAttack))
+	return ;
+}
+
+
+void	FragTrap::rangedAttack(std::string const & target) const
+{
+	std::cout << target << "Now, Get my rangeAttack in your face, shoesBox !" << std::endl;
+	return ;
+}
+
+void	FragTrap::meleeAttack(std::string const & target) const
+{
+	std::cout << target << "This is for Mathilda, shoesBox !" << std::endl;	
+	return ;
+}
+
+void	FragTrap::provokeAttack(std::string const & target) const
+{
+	std::cout << target << "Now, Get my rangeAttack in your face, " << target << std::right << "-20 Health_damage" << std::endl;
+	return ;
+}
+
+void	FragTrap::fingerAttack(std::string const & target) const
+{
+	std::cout << target << "This is for Mathilda, " << target << std::right << "-20 Health_damage" << std::endl;	
+	return ;
+}
+
+void	FragTrap::hammerAttack(std::string const & target) const
+{
+	std::cout << target << "This is for Mathilda, shoesBox !" << std::endl;	
+	return ;
+}
+
+void	FragTrap::takeDamage(unsigned int amount)
+{
+	if (_energyPoints - amount < 0)
+		_energyPoints = 0;
+	else
+		_energyPoints -= amount;
+	return ;
+}
+
+void	FragTrap::beRepaired(unsigned int amount)
+{
+	if (_energyPoints + amount > 100)
+		_energyPoints = 100;
+	else
+		_energyPoints += amount;
+	return ;
 }
 
 std::ostream &    operator<<( std::ostream & o, FragTrap const & i )
 {
-	o << "The value of _nb is : " << i.getNb();
+	o << i.getHealth();
 	return o;
 }
--- cpp03/ex00/FragTrap.hpp
+++ cpp03/ex00/FragTrap.hpp
@@ -8,26 +8,33 @@ class FragTrap
 {
 public:
 	FragTrap( void );
+	FragTrap( std::string name );
 	FragTrap( int const n );
 	FragTrap( FragTrap const & src );
 	~FragTrap( void );
 
 	FragTrap&	operator=( FragTrap const & rhs );
-	void	rangedAttack(std::string const & target);
-	void	meleeAttack(std::string const & target);
+	void	vaulthunter_dot_exe(std::string const & target);
+	void	rangedAttack(std::string const & target) const;
+	void	meleeAttack(std::string const & target) const;
+	void	provokeAttack(std::string const & target) const;
+	void	fingerAttack(std::string const & target) const;
+	void	hammerAttack(std::string const & target) const;
 	void	takeDamage(unsigned int amount);
 	void	beRepaired(unsigned int amount);
-	int		getNb(void) const;
+	int		getHealth(void) const;
 
 private:
-	int _hitPoints;
-	int _maxHitPoints;
-	int _maxHealthPoints;
-	int _level;
+	unsigned int _hitPoints;
+	unsigned int _maxHitPoints;
+	unsigned int _energyPoints;
+	unsigned int _maxEnergyPoints;
+	unsigned int _level;
 	std::string _name;
-	int _melleeAttackDamage;
-	int _rangedAttackDamage;
-	int _armorDamageReduction;
+	unsigned int _melleeAttackDamage;
+	unsigned int _rangedAttackDamage;
+	unsigned int _armorDamageReduction;
+	void (FragTrap::*_tabAttack[5])(std::string const & target) const; // https://isocpp.org/wiki/faq/pointers-to-members (cpp pointer on member function) https://www.tutorialspoint.com/function-pointer-to-member-function-in-cplusplus
 
 };
 
